836e1ace88cd653742364c86d5d3a03456d90049
Andre Silva <andre@particle41.com>
2021-08-11 15:24:47 -0300

    update examples

---
+++ examples/main.tf
@@ -0,0 +1,19 @@
+module "monitoring_resources" {
+  source = "github.com/andreswebs/terraform-aws-eks-monitoring"
+
+  cluster_oidc_provider = var.eks_cluster_oidc_provider
+
+  loki_iam_role_name           = "loki-${var.eks_cluster_id}"
+  loki_compactor_iam_role_name = "loki-compactor-${var.eks_cluster_id}"
+  grafana_iam_role_name        = "grafana-${var.eks_cluster_id}"
+
+  loki_storage_s3_bucket_name = var.loki_storage_s3_bucket_name
+
+  chart_version_loki_distributed = var.chart_version_loki_distributed
+  chart_version_promtail         = var.chart_version_promtail
+  chart_version_prometheus       = var.chart_version_prometheus
+  chart_version_grafana          = var.chart_version_grafana
+
+  grafana_enabled = true
+
+}
--- modules/iam/README.md
+++ modules/iam/README.md
@@ -9,11 +9,16 @@ Example:
 
 ```hcl
 module "monitoring_iam" {
-  source                      = "github.com/andreswebs/terraform-aws-eks-monitoring//modules/iam"
-  cluster_oidc_provider       = var.cluster_oidc_provider
+  source = "github.com/andreswebs/terraform-aws-eks-monitoring//modules/iam"
+
+  cluster_oidc_provider = var.eks_cluster_oidc_provider
+
+  loki_iam_role_name           = "loki-${var.eks_cluster_id}"
+  loki_compactor_iam_role_name = "loki-compactor-${var.eks_cluster_id}"
+  grafana_iam_role_name        = "grafana-${var.eks_cluster_id}"
+
   loki_storage_s3_bucket_name = var.loki_storage_s3_bucket_name
 }
-
 ```
 
 
