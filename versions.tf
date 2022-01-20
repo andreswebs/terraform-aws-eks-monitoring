ff22b0f97261af1ba647e1aaead399b61f2c27f4
Andre Silva <andreswebs@pm.me>
2022-01-20 17:51:49 -0300

    update versions

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.48.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.2.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+
+  }
+}
