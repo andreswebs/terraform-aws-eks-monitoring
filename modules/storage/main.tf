6df79cfae99dd4b3814f4f7d0a672d756e9979f5
Andre Silva <andreswebs@pm.me>
2023-03-14 16:55:30 -0300

    fix bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.arteinvisivel_com_br.id
+  bucket = aws_s3_bucket.this.id
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = local.kms_key_arn
