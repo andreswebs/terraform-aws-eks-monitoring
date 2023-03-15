5314b331a4d9454cb78fbff7d2a86fd9cf24ad7b
Andre Silva <andreswebs@pm.me>
2023-03-15 07:14:52 -0300

    add missing vars

--- variables.tf
+++ variables.tf
@@ -54,6 +54,18 @@ variable "loki_aggregator" {
   }
 }
 
+variable "loki_iam_role_name" {
+  type        = string
+  description = "Name of IAM role for Loki"
+  default     = "loki"
+}
+
+variable "loki_compactor_iam_role_name" {
+  type        = string
+  description = "Name of IAM role for Loki Compactor"
+  default     = "loki-compactor"
+}
+
 variable "create_loki_storage" {
   type        = bool
   description = "Create S3 bucket for Loki storage?"
@@ -126,6 +138,12 @@ variable "grafana_service_account_name" {
   default     = "grafana"
 }
 
+variable "grafana_iam_role_name" {
+  type        = string
+  description = "Name of IAM role for Grafana"
+  default     = "grafana"
+}
+
 ## chart versions
 
 variable "chart_version_metrics_server" {
@@ -171,3 +189,139 @@ variable "chart_version_loki" {
 }
 
 ## end chart versions
+
+## helm
+
+variable "helm_release_name_metrics_server" {
+  type        = string
+  description = "Release name"
+  default     = "metrics-server"
+}
+
+variable "helm_release_name_prometheus" {
+  type        = string
+  description = "Release name"
+  default     = "prometheus"
+}
+
+variable "helm_release_name_grafana" {
+  type        = string
+  description = "Release name"
+  default     = "grafana"
+}
+
+variable "helm_release_name_loki" {
+  type        = string
+  description = "Release name"
+  default     = "loki"
+}
+
+variable "helm_release_name_promtail" {
+  type        = string
+  description = "Release name"
+  default     = "promtail"
+}
+
+variable "helm_release_name_fluent_bit" {
+  type        = string
+  description = "Release name"
+  default     = "fluent-bit"
+}
+
+variable "helm_max_history" {
+  type        = number
+  description = "Maximum number of release versions stored per release; `0` means no limit"
+  default     = 3
+}
+
+variable "helm_timeout_seconds" {
+  type        = number
+  description = "Time in seconds to wait for any individual kubernetes operation"
+  default     = 300
+}
+
+variable "helm_recreate_pods" {
+  type        = bool
+  description = "Perform pods restart during upgrade/rollback ?"
+  default     = true
+}
+
+variable "helm_atomic_creation" {
+  type        = bool
+  description = "Purge resources on installation failure ? The wait flag will be set automatically if atomic is used"
+  default     = true
+}
+
+variable "helm_cleanup_on_fail" {
+  type        = bool
+  description = "Deletion new resources created in this upgrade if the upgrade fails ?"
+  default     = true
+}
+
+variable "helm_wait_for_completion" {
+  type        = bool
+  description = "Wait until all resources are in a ready state before marking the release as successful ?"
+  default     = true
+}
+
+variable "helm_wait_for_jobs" {
+  type        = bool
+  description = "Wait until all Jobs have been completed before marking the release as successful ?"
+  default     = true
+}
+
+variable "helm_verify" {
+  type        = bool
+  description = "Verify the package before installing it. Helm uses a provenance file to verify the integrity of the chart"
+  default     = false
+}
+
+variable "helm_keyring" {
+  type        = string
+  description = "Location of public keys used for verification; used only if verify is true"
+  default     = ".gnupg/pubring.gpg"
+}
+
+variable "helm_reuse_values" {
+  type        = bool
+  description = "When upgrading, reuse the last release's values and merge any overrides ? If 'reset_values' is specified, this is ignored"
+  default     = false
+}
+
+variable "helm_reset_values" {
+  type        = bool
+  description = "When upgrading, reset the values to the ones built into the chart ?"
+  default     = false
+}
+
+variable "helm_force_update" {
+  type        = bool
+  description = "Force resource update through delete/recreate if needed ?"
+  default     = false
+}
+
+variable "helm_create_namespace" {
+  type        = bool
+  description = "Create the namespace if it does not yet exist ?"
+  default     = true
+}
+
+variable "helm_replace" {
+  type        = bool
+  description = "Re-use the given name, even if that name is already used; this is unsafe in production"
+  default     = false
+}
+
+variable "helm_dependency_update" {
+  type        = bool
+  description = "Run helm dependency update before installing the chart ?"
+  default     = false
+}
+
+variable "helm_skip_crds" {
+  type        = bool
+  description = "Skip installing CRDs ?"
+  default     = false
+}
+
+## end helm
